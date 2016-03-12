77eff4d0396bd2bd1d8c9bbf33259b5a0a9f8208
Chintan <chintan@wohlig.com>
2016-03-12 10:58:05 +0530

    changes for the Backend

--- api/services/Suggestion.js
+++ api/services/Suggestion.js
@@ -8,95 +8,96 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
-    user: [{
-        type: Schema.ObjectId,
-        ref: 'User',
-        index: true
-  }],
-    name: String,
-    email: String,
-    contact: String,
-    subject: String,
-    query: String
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User',
+    index: true
+  },
+  name: String,
+  email: String,
+  contact: String,
+  subject: String,
+  query: String
 });
 
 module.exports = mongoose.model('Suggestion', schema);
 
 var models = {
-    //create
-    create: function (data, callback) {
-        var suggestion = this(data);
-        if (data._id) {
-            this.findOneAndUpdate({
-                _id: data._id
-            }, data, callback);
+  //create
+  create: function(data, callback) {
+    var suggestion = this(data);
+    if (data._id) {
+      this.findOneAndUpdate({
+        _id: data._id
+      }, data, callback);
+    } else {
+      suggestion.save(function(err, data) {
+        if (err) {
+          callback(err, false);
         } else {
-            suggestion.save(function (err, data) {
-                if (err) {
-                    callback(err, false);
-                } else {
-                    callback(null, data);
-                }
-            });
+          callback(null, data);
         }
-    },
+      });
+    }
+  },
 
-    // viewall
-    viewAll: function (data, callback) {
-        this.find().exec(callback);
-    },
+  // viewall
+  viewAll: function(data, callback) {
+    this.find().exec(callback);
+  },
 
-    //    view one
+  //    view one
 
-    view: function (data, callback) {
-        this.findOne({
-            "_id": data._id
-        }).exec(callback);
-    },
-    // delete
+  view: function(data, callback) {
+    this.findOne({
+      "_id": data._id
+    }).exec(callback);
+  },
+  // delete
 
-    delete: function (data, callback) {
-        this.findOneAndRemove({
-            _id: data._id
-        }, function (err, data) {
+  delete: function(data, callback) {
+    this.findOneAndRemove({
+      _id: data._id
+    }, function(err, data) {
 
-            if (err) {
-                callback(err, false);
-            } else {
-                callback(null, data);
-            }
-        });
-    },
+      if (err) {
+        callback(err, false);
+      } else {
+        callback(null, data);
+      }
+    });
+  },
 
-     findlimited: function (data, callback) {
-        var returnData = {};
-        var checkfor = new RegExp(data.search, "i");
-        var pagesize = parseInt(data.pagesize);
-        var pagenumber = parseInt(data.pagenumber);
-        async.parallel([
-            function (callback) {
-                Suggestion.count({
-                    name: {
-                        '$regex': checkfor
-                    }
-                }, callback);
-            },
-            function (callback) {
-                Suggestion.find({
-                    name: {
-                        '$regex': checkfor
-                    }
-                }, callback);
-            }], function (err, data2) {
-            if (err) {
-                callback(err, null);
-            } else {
-                returnData.totalpages = Math.ceil(data2[0] / pagesize);
-                returnData.total = data2[0];
-                returnData.data = data2[1];
-                callback(null, returnData);
-            }
-        });
-    },
+  findlimited: function(data, callback) {
+    var returnData = {};
+    var checkfor = new RegExp(data.search, "i");
+    var pagesize = parseInt(data.pagesize);
+    var pagenumber = parseInt(data.pagenumber);
+    async.parallel([
+      function(callback) {
+        Suggestion.count({
+          name: {
+            '$regex': checkfor
+          }
+        }, callback);
+      },
+      function(callback) {
+        Suggestion.find({
+          name: {
+            '$regex': checkfor
+          }
+        }, callback);
+      }
+    ], function(err, data2) {
+      if (err) {
+        callback(err, null);
+      } else {
+        returnData.totalpages = Math.ceil(data2[0] / pagesize);
+        returnData.total = data2[0];
+        returnData.data = data2[1];
+        callback(null, returnData);
+      }
+    });
+  },
 };
 module.exports = _.assign(module.exports, models);
--- api/services/Usernotification.js
+++ api/services/Usernotification.js
@@ -8,100 +8,101 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
-    user: [{
-        type: Schema.ObjectId,
-        ref: 'User',
-        index: true
-  }],
-    starttime: Date,
-    endtime: Date,
-    creationtime: Date,
-    modificationtime: Date,
-    status: String,
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User',
+    index: true
+  },
+  starttime: Date,
+  endtime: Date,
+  creationtime: Date,
+  modificationtime: Date,
+  status: String,
 });
 
 module.exports = mongoose.model('Usernotification', schema);
 
 var models = {
-    //create
-    create: function (data, callback) {
-        var usernotification = this(data);
-        if (data._id) {
-            this.findOneAndUpdate({
-                _id: data._id
-            }, data, callback);
+  //create
+  create: function(data, callback) {
+    var usernotification = this(data);
+    if (data._id) {
+      this.findOneAndUpdate({
+        _id: data._id
+      }, data, callback);
+    } else {
+      usernotification.save(function(err, data) {
+        if (err) {
+          callback(err, false);
         } else {
-            usernotification.save(function (err, data) {
-                if (err) {
-                    callback(err, false);
-                } else {
-                    callback(null, data);
-                }
-            });
+          callback(null, data);
         }
-    },
-    // viewall
-    viewAll: function (data, callback) {
-        this.find().exec(callback);
-    },
-    //    view one
+      });
+    }
+  },
+  // viewall
+  viewAll: function(data, callback) {
+    this.find().exec(callback);
+  },
+  //    view one
 
-    view: function (data, callback) {
-            this.findOne({
-                "_id": data._id
-            }).exec(callback);
-        },
-        // delete
+  view: function(data, callback) {
+    this.findOne({
+      "_id": data._id
+    }).exec(callback);
+  },
+  // delete
 
-        delete: function (data, callback) {
-        this.findOneAndRemove({
-            _id: data._id
-        }, function (err, data) {
+  delete: function(data, callback) {
+    this.findOneAndRemove({
+      _id: data._id
+    }, function(err, data) {
 
-            if (err) {
-                callback(err, false);
-            } else {
-                callback(null, data);
-            }
-        });
-    },
+      if (err) {
+        callback(err, false);
+      } else {
+        callback(null, data);
+      }
+    });
+  },
 
-    // view by user
+  // view by user
 
-    viewByUser: function (data, callback) {
-        this.findOne({
-            "user": data.user
-        }).populate('User').exec(callback);
-    },
-     findlimited: function (data, callback) {
-        var returnData = {};
-        var checkfor = new RegExp(data.search, "i");
-        var pagesize = parseInt(data.pagesize);
-        var pagenumber = parseInt(data.pagenumber);
-        async.parallel([
-            function (callback) {
-                Usernotification.count({
-                    name: {
-                        '$regex': checkfor
-                    }
-                }, callback);
-            },
-            function (callback) {
-                Usernotification.find({
-                    name: {
-                        '$regex': checkfor
-                    }
-                }, callback);
-            }], function (err, data2) {
-            if (err) {
-                callback(err, null);
-            } else {
-                returnData.totalpages = Math.ceil(data2[0] / pagesize);
-                returnData.total = data2[0];
-                returnData.data = data2[1];
-                callback(null, returnData);
-            }
-        });
-    },
+  viewByUser: function(data, callback) {
+    this.findOne({
+      "user": data.user
+    }).populate('User').exec(callback);
+  },
+  findlimited: function(data, callback) {
+    var returnData = {};
+    var checkfor = new RegExp(data.search, "i");
+    var pagesize = parseInt(data.pagesize);
+    var pagenumber = parseInt(data.pagenumber);
+    async.parallel([
+      function(callback) {
+        Usernotification.count({
+          name: {
+            '$regex': checkfor
+          }
+        }, callback);
+      },
+      function(callback) {
+        Usernotification.find({
+          name: {
+            '$regex': checkfor
+          }
+        }, callback);
+      }
+    ], function(err, data2) {
+      if (err) {
+        callback(err, null);
+      } else {
+        returnData.totalpages = Math.ceil(data2[0] / pagesize);
+        returnData.total = data2[0];
+        returnData.data = data2[1];
+        callback(null, returnData);
+      }
+    });
+  },
 };
 module.exports = _.assign(module.exports, models);
