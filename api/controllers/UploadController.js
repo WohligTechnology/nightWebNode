05aad7246065eddd0e3bbcb2ca363ba658190c12
Chintan <chintan@wohlig.com>
2016-03-12 14:40:52 +0530

    changes

--- api/controllers/UploadController.js
+++ api/controllers/UploadController.js
@@ -11,16 +11,17 @@ module.exports = {
       Config.GlobalCallback(err, fileNames, res);
     }
     var fileNames = [];
-    req.file("file").upload(function(err, uploadedFile) {
+    req.file("file").upload({
+      maxBytes: 10000000  // 10 MB Storage 1 MB = 10^6
+    }, function(err, uploadedFile) {
       async.each(uploadedFile, function(n, callback) {
         Config.uploadFile(n.fd, function(err, value) {
           if (err) {
             callback(err);
+          } else {
+            fileNames.push(value.name);
+            callback(null);
           }
-					else {
-						fileNames.push(value.name);
-						callback(null);
-					}
         });
       }, callback2);
 
