72e439f3af5ad0aff846cc2c69c25869011b9bbb
Pooja <pthakare33@gmail.com>
2016-03-12 19:06:15 +0530

    user controller sort

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -5,26 +5,27 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+
+
 module.exports = {
+  /**
+   * `UserController.create()`
+   */
+  create: function(req, res) {
+    function callback(err, data) {
+      Config.GlobalCallback(err, data, res);
+    }
+    if (req.body) {
+      User.create(req.body, callback);
+    } else {
+      res.json({
+        value: false,
+        comment: "No data found"
+      });
+    }
+  },
 
 
-
-    /**
-     * `UserController.create()`
-     */
-    create: function (req, res) {
-        function callback(err, data) {
-            Config.GlobalCallback(err, data, res);
-        }
-        if (req.body) {
-            User.create(req.body, callback);
-        } else {
-            res.json({
-                value: false,
-                comment: "No data found"
-            });
-        }
-    },
 
 
 
@@ -64,5 +65,38 @@ module.exports = {
             Config.GlobalCallback(err, data, res);
         }
         User.findlimited(req.body, callback);
+
+  /**
+   * `UserController.delete()`
+   */
+  delete: function(req, res) {
+    function callback(err, data) {
+      Config.GlobalCallback(err, data, res);
     }
+    User.delete(req.body, callback);
+  },
+
+
+  /**
+   * `UserController.viewAll()`
+   */
+  viewAll: function(req, res) {
+    function callback(err, data) {
+      Config.GlobalCallback(err, data, res);
+    }
+    User.viewAll(req.body, callback);
+  },
+
+
+  /**
+   * `UserController.view()`
+   */
+  view: function(req, res) {
+    function callback(err, data) {
+      Config.GlobalCallback(err, data, res);
+    }
+    User.view(req.body, callback);
+  },
+
+  
 };
